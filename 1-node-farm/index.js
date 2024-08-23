e54fc6b159672c21ac399410d6e3a92ee4860099
nodirbeksulaymonov <s.nodir9494@gmail.com>
2024-08-23 11:04:56 +0500

    feat: asynchronous way for read and write file

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,8 +1,25 @@
 const fs = require('fs');
 
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log(textIn);
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+//
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log('File written');
 
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`
-fs.writeFileSync("./txt/output.txt", textOut);
-console.log('File written');
+//No Blocking, asynchronous way
+fs.readFile('./txt/start.txt', 'utf8',(err, data1) => {
+    if (err) return console.log('Error 💥');
+    fs.readFile(`./txt/${data1}.txt`, 'utf8',(err, data2) => {
+        console.log(data2);
+        fs.readFile(`./txt/append.txt`, 'utf8',(err, data3) => {
+            console.log(data3);
+
+            fs.writeFile(`./txt/final.txt`,`${data2}\n${data3}`, `utf8`,err => {
+                console.log('You are file has been written to:\n');
+            })
+        });
+    });
+});
+console.log('will read file');
+
