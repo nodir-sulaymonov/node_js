f328f9cd69f33e44e90f40472e1780adcb295239
nodirbeksulaymonov <s.nodir9494@gmail.com>
2024-08-23 16:04:27 +0500

    feat: add routing

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,4 +1,9 @@
 const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+//////////////////////////////////////////////
+//FILES
 
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 // console.log(textIn);
@@ -8,19 +13,46 @@ const fs = require('fs');
 // console.log('File written');
 
 //No Blocking, asynchronous way
-fs.readFile('./txt/start.txt', 'utf8',(err, data1) => {
-    if (err) return console.log('Error 💥');
-    fs.readFile(`./txt/${data1}.txt`, 'utf8',(err, data2) => {
-        console.log(data2);
-        fs.readFile(`./txt/append.txt`, 'utf8',(err, data3) => {
-            console.log(data3);
+// fs.readFile('./txt/start.txt', 'utf8',(err, data1) => {
+//     if (err) return console.log('Error 💥');
+//     fs.readFile(`./txt/${data1}.txt`, 'utf8',(err, data2) => {
+//         console.log(data2);
+//         fs.readFile(`./txt/append.txt`, 'utf8',(err, data3) => {
+//             console.log(data3);
+//
+//             fs.writeFile(`./txt/final.txt`,`${data2}\n${data3}`, `utf8`,err => {
+//                 if (err) return console.log('Error 💥');
+//                 console.log('You are file has been written to:\n');
+//             })
+//         });
+//     });
+// });
+// console.log('will read file');
 
-            fs.writeFile(`./txt/final.txt`,`${data2}\n${data3}`, `utf8`,err => {
-                if (err) return console.log('Error 💥');
-                console.log('You are file has been written to:\n');
-            })
+
+///////////////////////////////////
+///SERVER
+
+const server = http.createServer((req, res) => {
+
+    const pathName=  req.url;
+
+    if (pathName === '/' || pathName === '/overview') {
+        res.end('This is overview');
+    } else if (pathName === '/product') {
+        res.end('This is product');
+    } else {
+        res.writeHead(404, {
+            'Content-Type': 'text/html',
+            'my-own-header': 'hello-world',
         });
-    });
+        res.end('<h1>Not Found</h1>');
+    }
 });
-console.log('will read file');
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening on 8000');
+});
+
+
 
